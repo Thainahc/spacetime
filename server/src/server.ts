b0435412e621543d36f0130870007cc840de6ebe
Thainá Helena <thainahelenadev@gmail.com>
2023-05-19 00:41:44 -0300

    Criação das rotas no backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,13 @@
-import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
 app
   .listen({
