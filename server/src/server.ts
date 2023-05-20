165ff42925b8cc8761dcd95dd423cd83e1bd81c0
Thainá Helena <thainahelenadev@gmail.com>
2023-05-20 15:57:07 -0300

    Autenticação das rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,21 @@
 import fastify from 'fastify'
 import { memoriesRoutes } from './routes/memories'
 import cors from '@fastify/cors'
+import 'dotenv/config'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(jwt, {
+  secret: '311adf5df1ds8rqrasfdafa8r42213158a4dsa',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
