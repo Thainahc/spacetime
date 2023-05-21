47e86e133c9bac385465401a071c0cbefeb726dd
Thainá Helena <thainahelenadev@gmail.com>
2023-05-21 00:36:54 -0300

    Rota para upload de arquivos

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
-import fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
-import cors from '@fastify/cors'
 import 'dotenv/config'
-import { authRoutes } from './routes/auth'
+
+import cors from '@fastify/cors'
+import fastify from 'fastify'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
 
@@ -15,7 +19,14 @@ app.register(jwt, {
   secret: '311adf5df1ds8rqrasfdafa8r42213158a4dsa',
 })
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
+app.register(multipart)
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
